b477626008b9ae63be60762238ddc551130f5f85
李光浩 <liguanghao@viatomtech.com>
2021-07-05 16:10:27 +0800

    ertg

--- app/src/main/java/com/vaca/alarmclock/AlarmReceiver.java
+++ app/src/main/java/com/vaca/alarmclock/AlarmReceiver.java
@@ -9,9 +9,9 @@ import android.widget.Toast;
 
 public class AlarmReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
-        if ("fuck".equals(intent.getAction())) {
+        if ("com.vaca.alarmclock.fuck".equals(intent.getAction())) {
             // 第1步中设置的闹铃时间到，这里可以弹出闹铃提示并播放响铃
-            Toast.makeText(context, "hello alarm", Toast.LENGTH_LONG).show();
+            Toast.makeText(context, intent.getAction(), Toast.LENGTH_SHORT).show();
             Log.e("fcuk","圣诞节快乐附近开了");
             // 可以继续设置下一次闹铃时间;
             return;
--- app/src/main/java/com/vaca/alarmclock/MainActivity.kt
+++ app/src/main/java/com/vaca/alarmclock/MainActivity.kt
@@ -5,20 +5,56 @@ import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.os.SystemClock
 import androidx.appcompat.app.AppCompatActivity
 import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-       val intent=Intent(this,Da::class.java);
-       val pend=PendingIntent.getService(this,0,intent,PendingIntent.FLAG_UPDATE_CURRENT); //显示闹钟，alarmActivity
+//       val intent=Intent(this,Da::class.java);
+//       val pend=PendingIntent.getService(this,0,intent,PendingIntent.FLAG_ONE_SHOT); //显示闹钟，alarmActivity
         val alarm= getSystemService(Context.ALARM_SERVICE) as (AlarmManager)     // 通过Context.ALARM_SERVICE获取AlarmManager对象
 
-        alarm.set(AlarmManager.RTC,System.currentTimeMillis()+1000,pend);     //设定闹钟
+//        alarm.set(AlarmManager.RTC_WAKEUP,System.currentTimeMillis()+1000,pend);     //设定闹钟
 
+//        alarm.setInexactRepeating(    AlarmManager.ELAPSED_REALTIME_WAKEUP,
+//            SystemClock.elapsedRealtime() + 2000,2000,pend);
+
+
+//        val intent=Intent("com.vaca.alarmclock.fuck")
+
+        val alarmIntent = Intent(this, AlarmReceiver::class.java).let { intent ->
+            intent.action="com.vaca.alarmclock.fuck"
+            PendingIntent.getBroadcast(this, 0, intent, 0)
+        }
+//        alarm.set(AlarmManager.RTC_WAKEUP,System.currentTimeMillis()+1000,alarmIntent);
+
+//
+//                alarm.setInexactRepeating(    AlarmManager.ELAPSED_REALTIME_WAKEUP,
+//            SystemClock.elapsedRealtime() + 2000,2000,alarmIntent);
+
+
+
+        val calendar: Calendar = Calendar.getInstance().apply {
+            timeInMillis = System.currentTimeMillis()
+            set(Calendar.HOUR_OF_DAY, 10)
+            set(Calendar.MINUTE, 7)
+        }
+
+// setRepeating() lets you specify a precise custom interval--in this case,
+// 20 minutes.
+        alarm.setRepeating(
+            AlarmManager.RTC_WAKEUP,
+            System.currentTimeMillis(),
+            1000 *  60*2,
+            alarmIntent
+        )
     }
 
 
